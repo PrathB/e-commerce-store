6816551fa340f31c253e34a5d4781e487b0cb8fe
Pratham Batra <pratham11.batra@gmail.com>
2024-07-29 14:33:16 +0530

    initial commit

---
+++ src/customer/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,14 @@
+export const mainCarouselData = [
+    {
+        image:"https://upload.wikimedia.org/wikipedia/commons/a/af/Car_repair_shop.jpg"
+    }
+    ,
+    {
+        image:"https://upload.wikimedia.org/wikipedia/commons/a/af/Car_repair_shop.jpg"
+    }
+    ,
+    {
+        image:"https://upload.wikimedia.org/wikipedia/commons/a/af/Car_repair_shop.jpg"
+    }
+
+]
