23c5954b61ebf5ff4c10278d8265f75a3b8bdc17
Pratham Batra <pratham11.batra@gmail.com>
2024-08-11 16:38:06 +0530

    Added home section carousel to display products

--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import MainCarousel from '../../HomeCarousel/MainCarousel'
+import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+import HomeSectionCarousel from '../../components/HomeSectionCarousel/HomeSectionCarousel'
 
 const HomePage = () => {
   return (
     <div>
       <MainCarousel/>
 
-      <div>
-        other section
+      <div className='space-y-10 py-20 flex flex-col justify-center px-5 lg:px-10'>
+        <HomeSectionCarousel/>
+        <HomeSectionCarousel/>
+        <HomeSectionCarousel/>
       </div>
     </div>
   )
