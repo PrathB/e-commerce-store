61cac685d7428d7cb0142d1487246c5d6359c6b5
Pratham Batra <pratham11.batra@gmail.com>
2024-08-23 13:22:14 +0530

    Added product page and product card

--- src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -1,8 +1,9 @@
+import { BorderColor } from "@mui/icons-material";
 import React from "react";
 
 const HomeSectionCard = ({ product }) => {
   return (
-    <div className="cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3 border">
+    <div className="cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3 mb-2 border py-2 hover:border-[#7f0000]">
       <div className="h-[13rem] w-[10rem]">
         <img
           className="object-cover w-full h-full"
@@ -12,8 +13,8 @@ const HomeSectionCard = ({ product }) => {
       </div>
 
       <div className="p-4">
-        <h3 className="text-lg font-medium text-gray-900">{product.title}</h3>
-        <h3 className="text-lg font-bold text-black mt-2 ">
+        <h3 className="font-medium text-gray-800">{product.title}</h3>
+        <h3 className="text-lg font-medium text-black mt-2 ">
           Rs {product.price}
         </h3>
       </div>
